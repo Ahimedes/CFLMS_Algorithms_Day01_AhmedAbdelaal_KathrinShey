2c01714c93ae17f73eccb12d63e8ea6041ea9edd
Ahmed Abdelaal <ahmed.abdellaoue@email.com>
2020-05-20 15:02:02 +0200

    uploads 3 exercises

---
+++ pseudo1.js
@@ -0,0 +1,25 @@
+
+INPUT User inputs an int and saves as variable N
+
+max_number = (int) User Input 
+
+OUTPUT CPU chooses int between 0 and N and saves as variable
+
+choice = (int) CPU 
+
+INPUT int thats between 0 and N 
+
+guess = (int) User Input
+
+IF guess < choice 
+Output = “My number is higher than "guess", guess again higher!”
+
+ELSE IF guess > choice
+Output = “My number is lower than "guess", guess again lower!”
+
+ELSE 
+Output = “You are correct - the random number is "guess"”
+
+END IF
+
+
